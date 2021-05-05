0908120ba5d1f0a1f8ffae9b3fdddacf38e2bbd2
Journey Gault <journey@Journeys-MacBook-Pro.local>
2021-05-05 11:53:12 -0400

    uploading file

---
+++ A4-components-logic.js
@@ -0,0 +1,26 @@
+﻿window.onload = function() {
+
+    
+     class HumberClock extends HTMLElement{
+        constructor(){
+            super();
+            let template = document.getElementById("HumberClockTemplate")
+            let templateContent = template.content;
+            const shadowRoot = this.attachShadow({mode:'open'}).appendChild(templateContent.cloneNode(true));
+        }
+    }
+        customElements.define('humber-clock',HumberClock)
+
+
+    var currentTime= document.getElementById("time");
+	//setInterval((currentTime.innerHTML= JourneysNS.SendFunction()),1000)
+   
+    setInterval(showtime,1000)
+    function showtime(){
+    currentTime.innerHTML= JourneysNS.SendFunction()
+}
+    //setInterval((currentTime.innerHTML= JourneysNS.SendFunction()),1000)
+}
+    
+
+    
---
+++ A4-components-module.js
@@ -0,0 +1,36 @@
+﻿var JourneysNS = (function () {
+
+	
+	function displayTime(){
+		//LOGIC GOES HERE
+	var now = new Date();
+	var hoursValue = now.getHours();
+	var minutesValue= now.getMinutes();
+	var secondsValue = now.getSeconds();
+		hours= hoursValue +":";
+		if (hoursValue<=9){
+			minutes="0"+ minutesValue+":";
+		}else{
+			minutes= minutesValue+":";
+		}
+		if (minutesValue<=9){
+			minutes= "0" + minutesValue+":";
+		}else{
+			minutes= minutesValue+":";
+		}
+		if (secondsValue<=9){
+			seconds= "0"+ secondsValue;
+		}else{
+		seconds= secondsValue;
+		}
+		var time = hours+minutes+seconds;
+		//var time = "test";
+		//YOU NEED TO RETURN SOMETHING TO BE RETURNED
+		return time 
+	}
+
+        var module = {
+        SendFunction : displayTime // THIS IS WHAT IS BEING SENT
+    };
+    return module;
+})(); 
